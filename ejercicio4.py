e1931abb81dd72335bd71e50ea67768ad7183989
Dv-AlejandroEH <aespher121@gmail.com>
2025-01-28 18:39:56 +0100

    ejercicio4 finished

---
+++ ejercicio4.py
@@ -0,0 +1,22 @@
+def recibir_lista():
+    lista_numeros = []
+    while True:
+        numero = int(input('Introduzca los números (Introduzca "0" para dejar de recibir números): '))
+        if numero == 0:
+            break
+        lista_numeros.append(numero)
+    return lista_numeros
+def calcular_suma(lista_numeros):
+    suma = 0
+    for i in range(len(lista_numeros)):
+        suma += lista_numeros[i]
+    return suma
+def calcular_promedio(lista_numeros, suma):
+    promedio = suma / len(lista_numeros)
+    return int(promedio)
+def imprimir_por_pantalla():
+    lista_numeros = recibir_lista()
+    suma = calcular_suma(lista_numeros)
+    promedio = calcular_promedio(lista_numeros, suma)
+    print('La suma de los números es:', suma, 'y el promedio de los números es:', promedio)
+imprimir_por_pantalla()
