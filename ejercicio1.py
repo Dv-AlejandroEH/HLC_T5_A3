a0c4517d7c3dda5ca5a0b5e93bb19d8f38c5b858
Dv-AlejandroEH <aespher121@gmail.com>
2025-01-29 12:45:39 +0100

    Refactor A1, A4

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,10 +1,16 @@
+# def ordenar_numeros(numeros_lista):
+#     numeros_lista.sort()
+#     print(numeros_lista)
+# numeros= []
+# while True:
+#     numeros.append(int(input('Introduzca números para ser ordenados (escriba 0 para terminar de escribir): ')))
+#     if numeros[-1] == 0:
+#         numeros.remove('exit')
+#         break
+# ordenar_numeros(numeros)
+
 def ordenar_numeros(numeros_lista):
     numeros_lista.sort()
     print(numeros_lista)
-numeros= []
-while True:
-    numeros.append(int(input('Introduzca números para ser ordenados (escriba 0 para terminar de escribir): ')))
-    if numeros[-1] == 0:
-        numeros.remove('exit')
-        break
+numeros= [5, 2, 9, 1, 6]
 ordenar_numeros(numeros)
--- ejercicio4.py
+++ ejercicio4.py
@@ -1,10 +1,29 @@
+# def recibir_lista():
+#     lista_numeros = []
+#     while True:
+#         numero = int(input('Introduzca los números (Introduzca "0" para dejar de recibir números): '))
+#         if numero == 0:
+#             break
+#         lista_numeros.append(numero)
+#     return lista_numeros
+# def calcular_suma(lista_numeros):
+#     suma = 0
+#     for i in range(len(lista_numeros)):
+#         suma += lista_numeros[i]
+#     return suma
+# def calcular_promedio(lista_numeros, suma):
+#     promedio = suma / len(lista_numeros)
+#     return int(promedio)
+# def imprimir_por_pantalla():
+#     lista_numeros = recibir_lista()
+#     suma = calcular_suma(lista_numeros)
+#     promedio = calcular_promedio(lista_numeros, suma)
+#     print('La suma de los números es:', suma, 'y el promedio de los números es:', promedio)
+# imprimir_por_pantalla()
+
+
 def recibir_lista():
-    lista_numeros = []
-    while True:
-        numero = int(input('Introduzca los números (Introduzca "0" para dejar de recibir números): '))
-        if numero == 0:
-            break
-        lista_numeros.append(numero)
+    lista_numeros = [10, 20, 30]
     return lista_numeros
 def calcular_suma(lista_numeros):
     suma = 0
