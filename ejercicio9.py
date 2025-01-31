6aac8b486dbdf5dc29331d0dd6de5a1e7dc69400
Dv-AlejandroEH <aespher121@gmail.com>
2025-01-31 12:51:43 +0100

    finished E9

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,7 +1,14 @@
 def calcular_alumnos():
     alumnos = {'Juan': 7, 'Ana': 9, 'Pedro': 6}
-    promedio = alumnos.keys / len(alumnos)
-    for i in alumnos.keys:
-        
-
-print(calcular_alumnos)
+    promedio = 0
+    nota_mayor = 0
+    mayor = ''
+    for alumno, nota in alumnos.items():
+        promedio += nota
+        if nota > nota_mayor:
+            nota_mayor = nota
+            mayor = alumno
+    promedio = promedio / len(alumnos)
+    return 'Promedio: ' + str(round(promedio, 2)) + ', Mejor estudiante: ' + mayor + ' con '+ str(nota_mayor)
+resultado = calcular_alumnos()
+print(resultado)
